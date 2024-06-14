80c9d027c65707a67d516548f9a3cc1398d12971
shunxiangmeng <shunxiangmeng@gmail.com>
2024-06-14 09:19:08 +0800

    update

--- alg/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <chrono>
-#include <thread>
-
-int main(int argc, char* argv[]) {
-    while (true) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
-    return 0;
-}
